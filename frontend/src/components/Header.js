d238c8dcf9aaf878e2953c8839874abd0c61148b
asifmanowar9 <raiyanasif939@gmail.com>
2025-04-29 22:48:01 +0600

    2nd day

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header>
+      <Navbar
+        bg='dark'
+        variant='dark'
+        expand='lg'
+        //className='bg-body-tertiary'
+        collapseOnSelect
+      >
+        <Container>
+          <Navbar.Brand href='/'>Medi-ghor</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
